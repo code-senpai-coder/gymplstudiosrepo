a50ca5fea2d657f7bc75934a68a64490063e6951
code-senpai-coder <matyasbalcar0@gmail.com>
2020-06-03 22:35:42 +0200

    adding all files to github

---
+++ main.js
@@ -0,0 +1,11 @@
+function swapElement(elementID){
+    
+    if(elementID.style.display === "none") 
+    {
+    elementID.style.display = "block";
+    } 
+    else 
+    {
+    elementID.style.display = "none";
+    } 
+}
