0e7158d4967b7330b175f0aabbfc9f8ad2568f1d
Code-senpai-coder <Matyasbalcar0@gmail.com>
2020-06-05 21:15:33 +0200

    Chnged syntax to es6 syntax

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function swapElement(elementID){
-    //hello thisbis test
-    elementID.style.display == "none" ? elementID.style.display = "block" : elementID.style.display == "none"
+const swapElement = (elementID) =>{
+  elementID.style.display == "none" ? 
+  elementID.style.display = "block" : 
+  elementID.style.display = "none"
 }
-
-const scrlt = (x,y) => {window.scroll(x,y)}
-
-//tady je odkaz na stack overflow na download udělej ještě dnes : https://stackoverflow.com/questions/16197706/how-to-save-javascript-variable-as-file-on-the-client-pc
+const scrlt = (x,y) => {window.scroll(x,y)};
