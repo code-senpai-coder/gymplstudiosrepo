4570d557f84f61765f04911c22ccddce18791172
code-senpai-coder <matyasbalcar0@gmail.com>
2020-06-05 14:36:27 +0200

    changing swap element syntax to es6 sytax

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 function swapElement(elementID){
     
-    if(elementID.style.display === "none") 
-    {
-    elementID.style.display = "block";
-    } 
-    else 
-    {
-    elementID.style.display = "none";
-    } 
+    elementID.style.display == "none" ? elementID.style.display = "block" : elementID.style.display == "none"
 }
 
 function scrlt(x,y){
