d582ccdd217207e9dd651cb23b7a12837f9e63f6
code-senpai-coder <matyasbalcar0@gmail.com>
2020-06-05 14:38:49 +0200

    changing swap element and scrlt  syntax to es6 sytax

--- main.js
+++ main.js
@@ -3,7 +3,4 @@ function swapElement(elementID){
     elementID.style.display == "none" ? elementID.style.display = "block" : elementID.style.display == "none"
 }
 
-function scrlt(x,y){
-    window.scrollTo(x,y)
-    //console.log("done")
-}
+const scrlt = (x,y) => {window.scroll(x,y)}
